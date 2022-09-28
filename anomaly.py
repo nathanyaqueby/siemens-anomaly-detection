d9ddda4b901e795c41f06fda547aa0d7074467d2
Nathanya Queby S <nathanyaqueby21@gmail.com>
2022-09-28 21:11:16 +0200

    Update anomaly.py

--- anomaly.py
+++ anomaly.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 st.title("Anomaly Detection Team - Challenge 4")
