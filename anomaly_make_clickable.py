f9d7207afc249f94520965fdb8e298d5c022697e
AnneKrus <95288023+AnneKrus@users.noreply.github.com>
2022-09-29 13:24:46 +0200

    .

--- anomaly_make_clickable.py
+++ anomaly_make_clickable.py
@@ -60,7 +60,13 @@ if uploaded_file is not None:
             # width=1200, height=400)
             # st.plotly_chart(state_total_graph, use_container_width=True)
             # Writes a component similar to st.write()
-            fig = px.line(x=[1], y=[1])
+            fig = px.line(
+            state_total, 
+            x='Date',
+            y='Value',
+            labels={'Value':'Value in %s' % (select)},
+            width=1200, height=400)
+            st.plotly_chart(fig, use_container_width=True)
             selected_points = plotly_events(fig)
 
             # Can write inside of things using with!
