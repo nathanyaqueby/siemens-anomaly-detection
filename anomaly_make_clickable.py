a27c867433f0a3471d1df72d306241eefbdbd4e4
AnneKrus <95288023+AnneKrus@users.noreply.github.com>
2022-09-29 16:57:31 +0200

    .

--- anomaly_make_clickable.py
+++ anomaly_make_clickable.py
@@ -30,60 +30,65 @@ if uploaded_file is not None:
         lcb = "LCB"
 
     st.sidebar.title("2. Location")
-    select = st.sidebar.selectbox('Select a location', df[lcb])
 
-    #get the state selected in the selectbox
-    state_data = df[df[lcb] == select]
-
-    countries=df[lcb].unique()
-    dic = {}
-    for country in countries:
-        dic[country]=df[df[lcb]==country]
-
-    def get_total_dataframe(dataset):
-        total_dataframe = pd.DataFrame({
-        'Date':dataset[date],
-        'Value':dataset[val]})
-        return total_dataframe
-
-    state_total = get_total_dataframe(state_data)
-    # state_total = ctr_data
+    check = st.sidebar.checkbox("Show analysis by location", value=False, key=2)
 
 
-    check = st.sidebar.checkbox("Show analysis by location", value=True, key=2)
 
-    if not st.checkbox('Hide graph', False, key=1):
-        if check:
-            st.markdown("## **Location analysis**")
-            date_min=state_total.Date.iloc[0].strftime("%B %Y")
-            date_max=state_total.Date.iloc[-1].strftime("%B %Y")
-            # st.markdown(f"### Overall {df_type} data in {select} from {date_min} to {date_max}")
-        
-            fig1 = px.line(
-            state_total, 
-            x='Date',
-            y='Value',
-            labels={'Value':'Value in %s' % (select)},
-            width=1200, height=400,
-            title=f"{df_type} data in {select} from {date_min} to {date_max}")
-            fig1.update(layout=dict(title=dict(x=0.5)))
-            # create list of dicts with selected points, and plot
-            selected_points = plotly_events(fig1)
-            # generate image for pdf
-            pio.write_image(fig1, "fig1.png", format="png", validate="False", engine="kaleido")
-        else:
-            date_min=df.Date.iloc[0].strftime("%B %Y")
-            date_max=df.Date.iloc[-1].strftime("%B %Y")
-            fig2 = px.line(df, x='Date', y='Value', color=lcb, title=f"All {df_type} data from {date_min} to {date_max}")
-            fig2.update(layout=dict(title=dict(x=0.5)))
-            selected_points = plotly_events(fig2)
-            pio.write_image(fig2, "fig1.png", format="png", validate="False", engine="kaleido")
 
-        # if a point was clicked, show info
-        if selected_points:
-            st.markdown("#### **Selected point**")
-            st.markdown("Date: {}".format(selected_points[0]["x"]))
-            st.markdown("Value: {}".format(selected_points[0]["y"]))
+    # if not st.checkbox('Hide graph', False, key=1):
+    if check:
+        # Add additional dropdown in sidebar
+        select = st.sidebar.selectbox('Select a location', df[lcb])
+
+        #get the state selected in the selectbox
+        state_data = df[df[lcb] == select]
+
+        countries=df[lcb].unique()
+        dic = {}
+        for country in countries:
+            dic[country]=df[df[lcb]==country]
+
+        def get_total_dataframe(dataset):
+            total_dataframe = pd.DataFrame({
+            'Date':dataset[date],
+            'Value':dataset[val]})
+            return total_dataframe
+
+        state_total = get_total_dataframe(state_data)
+
+        # Show figure per location
+        st.markdown("## **Location analysis**")
+        date_min=state_total.Date.iloc[0].strftime("%B %Y")
+        date_max=state_total.Date.iloc[-1].strftime("%B %Y")
+        # st.markdown(f"### Overall {df_type} data in {select} from {date_min} to {date_max}")
+    
+        fig1 = px.line(
+        state_total, 
+        x='Date',
+        y='Value',
+        labels={'Value':'Value in %s' % (select)},
+        width=1200, height=400,
+        title=f"{df_type} data in {select} from {date_min} to {date_max}")
+        fig1.update(layout=dict(title=dict(x=0.5)))
+        # create list of dicts with selected points, and plot
+        selected_points = plotly_events(fig1)
+        # generate image for pdf
+        pio.write_image(fig1, "fig1.png", format="png", validate="False", engine="kaleido")
+    else:
+        # Show figure of all data
+        date_min=df.Date.iloc[0].strftime("%B %Y")
+        date_max=df.Date.iloc[-1].strftime("%B %Y")
+        fig2 = px.line(df, x='Date', y='Value', color=lcb, title=f"All {df_type} data from {date_min} to {date_max}")
+        fig2.update(layout=dict(title=dict(x=0.5)))
+        selected_points = plotly_events(fig2)
+        pio.write_image(fig2, "fig1.png", format="png", validate="False", engine="kaleido")
+
+    # if a point was clicked, show info
+    if selected_points:
+        st.markdown("#### **Selected point**")
+        st.markdown("Date: {}".format(selected_points[0]["x"]))
+        st.markdown("Value: {}".format(selected_points[0]["y"]))
 
     # download as PDF
     st. markdown("### **Save to pdf**")
