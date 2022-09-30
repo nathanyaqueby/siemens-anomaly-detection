26fffbf391eaffd5235021650df17f99eebd7866
AnneKrus <95288023+AnneKrus@users.noreply.github.com>
2022-09-30 14:01:21 +0200

    .

--- anomaly_make_clickable.py
+++ anomaly_make_clickable.py
@@ -284,7 +284,7 @@ if uploaded_file is not None:
 
             # add anomalies in scatter form
             anomalies = result[result["Anomaly"]==True]
-            st.write(result.dtypes)
+            st.write(type(result))
             st.write(state_total.head())
             fig_temp = px.bar(anomalies, x="Date", y="y")
             fig1.add_trace(fig_temp.data[0])
