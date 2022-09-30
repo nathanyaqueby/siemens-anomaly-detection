91a5e92b405dcce6120f5ec9275249603f69b413
AnneKrus <95288023+AnneKrus@users.noreply.github.com>
2022-09-30 14:13:43 +0200

    .

--- anomaly_make_clickable.py
+++ anomaly_make_clickable.py
@@ -144,7 +144,7 @@ def fit_predict_model(m_path, dataframe):
     return forecast,result
 
 # new ARIMA extra function
-def analyze2(dic,name):
+def analyze2(dic,name,country):
     fpr, tpr, thresholds = metrics.roc_curve(dic[country]['Label_num'].values, result['Label_pred_num'].values) 
     TN, FP, FN, TP = confusion_matrix(dic[country]['Label_num'].values, result['Label_pred_num'].values).ravel() 
     acc= (TP+TN)/(TP+TN+FP+FN)
@@ -274,7 +274,7 @@ if uploaded_file is not None:
             evaluation = {}
             if test_stationarity(dic[select], 'y')=='Stationary':
                 pred,result = fit_predict_model(m_path, dic[select])
-                output = analyze2(dic, df_type)
+                output = analyze2(dic, df_type, select)
             else:
                 output={}
                 output['max']=0
